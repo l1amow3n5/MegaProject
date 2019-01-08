8deb0e683f266149eb0c3eb12386454f7d338398
Liam Owen <l1amow3n5@yahoo.com>
2019-01-08 12:44:31 -0700

    made connections from each button to its corresponding viewController

---
+++ MegaProject/Controller/MenuViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MenuViewController.swift
+//  MegaProject
+//
+//  Created by Owen, Liam on 1/8/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class MenuViewController: UIViewController
+{
+    
+    
+    
+    
+    
+
+    public override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
